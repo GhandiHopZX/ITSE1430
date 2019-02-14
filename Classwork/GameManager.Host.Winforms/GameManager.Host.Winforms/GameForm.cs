a493630dacc1d33c28d4897b1e37acfde4d9dd8f
solomon pattee <solomon.pattee@my.tccd.edu>
2019-02-13 21:48:47 -0600

    all messed up

--- Classwork/GameManager.Host.Winforms/GameManager.Host.Winforms/GameForm.cs
+++ Classwork/GameManager.Host.Winforms/GameManager.Host.Winforms/GameForm.cs
@@ -1,20 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GameManager.Host.Winforms
 {
+    /// <summary>Allows adding or editing a game.</summary>
     public partial class GameForm : Form
     {
         public GameForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>Gets or sets the property being edited.</summary>
+        public Game Game { get; set; }
+
+        //Called when the user saves the game
+        private void OnSave( object sender, EventArgs e )
+        {
+            Game = SaveData();
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        //Called when the user cancels the add/edit
+        private void OnCancel( object sender, EventArgs e )
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private decimal ReadDecimal( TextBox control )
+        {
+            if (Decimal.TryParse(control.Text, out var value))
+                return value;
+
+            return -1;
+        }
+
+        //Loads UI with game
+        private void LoadData( Game game )
+        {
+            en_Name.Text = game.Name;
+            en_Publisher.Text = game.Publisher;
+            en_Price.Text = game.Price.ToString();
+            _cbOwned.Checked = game.Owned;
+            _cbCompleted.Checked = game.Completed;
+        }
+
+        //Saves UI into new game
+        private Game SaveData()
+        {
+            var game = new Game();
+            game.Name = en_Name.Text;
+            game.Publisher = en_Publisher.Text;
+            game.Price = ReadDecimal(en_Price);
+            game.Owned = en_Owned.Checked;
+            game.Completed = en_Completed.Checked;
+
+            return game;
+        }
+
+        private void GameForm_Load( object sender, EventArgs e )
+        {
+            //Init UI if editing a game
+            if (Game != null)
+                LoadData(Game);
         }
     }
 }
--- Classwork/GameManager.Host.Winforms/GameManager.Host.Winforms/MainForm.Designer.cs
+++ Classwork/GameManager.Host.Winforms/GameManager.Host.Winforms/MainForm.Designer.cs
@@ -28,220 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this._miFileExit = new System.Windows.Forms.ToolStripMenuItem();
-            this.gamesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this._miGamesAdd = new System.Windows.Forms.ToolStripMenuItem();
-            this._miGamesEdit = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this._miGamesDelete = new System.Windows.Forms.ToolStripMenuItem();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this._miHelpAbout = new System.Windows.Forms.ToolStripMenuItem();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.menuStrip1.SuspendLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.gamesToolStripMenuItem,
-            this.helpToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(536, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // fileToolStripMenuItem
-            // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this._miFileExit});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "&File";
-            // 
-            // _miFileExit
-            // 
-            this._miFileExit.Name = "_miFileExit";
-            this._miFileExit.Size = new System.Drawing.Size(92, 22);
-            this._miFileExit.Text = "E&xit";
-            this._miFileExit.Click += new System.EventHandler(this.OnFileExit);
-            // 
-            // gamesToolStripMenuItem
-            // 
-            this.gamesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this._miGamesAdd,
-            this._miGamesEdit,
-            this.toolStripSeparator1,
-            this._miGamesDelete});
-            this.gamesToolStripMenuItem.Name = "gamesToolStripMenuItem";
-            this.gamesToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
-            this.gamesToolStripMenuItem.Text = "&Games";
-            // 
-            // _miGamesAdd
-            // 
-            this._miGamesAdd.Name = "_miGamesAdd";
-            this._miGamesAdd.Size = new System.Drawing.Size(107, 22);
-            this._miGamesAdd.Text = "&Add";
-            // 
-            // _miGamesEdit
-            // 
-            this._miGamesEdit.Name = "_miGamesEdit";
-            this._miGamesEdit.Size = new System.Drawing.Size(107, 22);
-            this._miGamesEdit.Text = "&Edit";
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(104, 6);
-            // 
-            // _miGamesDelete
-            // 
-            this._miGamesDelete.Name = "_miGamesDelete";
-            this._miGamesDelete.Size = new System.Drawing.Size(107, 22);
-            this._miGamesDelete.Text = "&Delete";
-            // 
-            // helpToolStripMenuItem
-            // 
-            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this._miHelpAbout});
-            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
-            this.helpToolStripMenuItem.Text = "&Help";
-            // 
-            // _miHelpAbout
-            // 
-            this._miHelpAbout.Name = "_miHelpAbout";
-            this._miHelpAbout.Size = new System.Drawing.Size(107, 22);
-            this._miHelpAbout.Text = "&About";
-            this._miHelpAbout.Click += new System.EventHandler(this.OnHelpAbout);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(57, 64);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Name";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(57, 143);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Publisher";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(57, 217);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Price";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(130, 64);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 6;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(130, 143);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 7;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(130, 279);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(66, 17);
-            this.checkBox1.TabIndex = 8;
-            this.checkBox1.Text = "Owned?";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(130, 353);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(82, 17);
-            this.checkBox2.TabIndex = 9;
-            this.checkBox2.Text = "Completed?";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(130, 217);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 10;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(257, 390);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Save";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(360, 390);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 427);
-            this.ControlBox = false;
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
+            resources.ApplyResources(this, "$this");
             this.Name = "MainForm";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Game Manager";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -257,16 +51,16 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem _miGamesDelete;
         private System.Windows.Forms.ToolStripMenuItem _miHelpAbout;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label _enName;
+        private System.Windows.Forms.Label _enPublisher;
+        private System.Windows.Forms.Label _enPrice;
+        private System.Windows.Forms.TextBox en_Name;
+        private System.Windows.Forms.TextBox en_Publisher;
+        private System.Windows.Forms.CheckBox _cbOwned;
+        private System.Windows.Forms.CheckBox _cbCompleted;
+        private System.Windows.Forms.TextBox en_Price;
+        private System.Windows.Forms.Button SaveBox;
+        private System.Windows.Forms.Button Cancel;
     }
 }
 
