74ada32cd749a45532c0f4a163783ce6823a53ff
solomon pattee <solomon.pattee@my.tccd.edu>
2019-03-06 21:37:47 -0600

    ok

--- Labs/Lab2/CharacterCreator.Winforms/CharacterCreator/Character.cs
+++ Labs/Lab2/CharacterCreator.Winforms/CharacterCreator/Character.cs
@@ -9,18 +9,19 @@ namespace CharacterCreator
     public class Character
     {
         //values for the character attributes
-      
-        private string _name { get; set; }
-        string[] _profession;
-        string[] _race;
-        //stats//
-        private int _strength { get; set; }
-        private int _intelligence { get; set; }
-        private int _agility { get; set; }
-        private int _constitution { get; set; }
-        private int _charisma { get; set; }
 
-        public int min { get; set; }
-        public int max { get; set; }
+        private string _name;
+        private string[] _profession;
+        private string[] _race;
+
+        //stats//
+        private int _strength;
+        private int _intelligence;
+        private int _agility;
+        private int _constitution;
+        private int _charisma;
+
+        const int _min = 0;
+        const int _max = 100;
     }
 }
