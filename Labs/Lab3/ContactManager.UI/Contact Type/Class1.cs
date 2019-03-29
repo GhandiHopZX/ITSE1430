3e18b98c670615c250728abd49e9e69995cc652c
Solomon Pattee <solomon.pattee@my.tccd.edu>
2019-03-28 20:55:10 -0500

    ok

--- Labs/Lab3/ContactManager.UI/Contact Type/Class1.cs
+++ Labs/Lab3/ContactManager.UI/Contact Type/Class1.cs
@@ -8,9 +8,77 @@ namespace Contact_Type
 {
     public class Contact : IContactDatabase
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        
+        public int Id { get; set; }
+        public string Name
+        {
+            get { return name ?? ""; }
+            set { name = value ?? ""; }
+        }
+        public string Email
+        {
+            get { return email ?? ""; }
+            set { email = value ?? ""; }
+        }
 
+        public IEnumerable<ValidationResult> Validate( ValidationContext validationContext )
+        {
+            var items = new List<ValidationResult>();
+
+            //Name is required
+            if (String.IsNullOrEmpty(Name))
+                items.Add(new ValidationResult("Name is required.", new[] { nameof(Name) }));
+
+            //Email Address is required
+            if (String.IsNullOrEmpty(Email))
+                items.Add(new ValidationResult("Email is required.", new[] { nameof(Email) }));
+
+            return items;
+        }
+
+        public Contact Add( Contact contact )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete( int id )
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact Get( int id )
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Contact> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact Update( int id, Contact game )
+        {
+            throw new NotImplementedException();
+        }
+
+        private string name { get; set; }
+
+        private string email { get; set; }
+
+    }
+
+    public class ValidationContext
+    {
+    }
+
+    public class ValidationResult
+    {
+        private string v1;
+        private string[] v2;
+
+        public ValidationResult( string v1, string[] v2 )
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
     }
 }
--- Labs/Lab3/ContactManager.UI/Contact Type/IContactDatabase.cs
+++ Labs/Lab3/ContactManager.UI/Contact Type/IContactDatabase.cs
@@ -1,27 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Contact_Type
 {
     public interface IContactDatabase
     {
-        //bool Validator
-        //{
-        //        if (String.IsNullOrEmpty(Name))
-        //        {
-        //            throw new ArgumentNullException(nameof(Name));
-        //        };
-        //        return false;
-
-        //    {/* Name = ""; Email = ""; */}
-        //}
-
-        /// <summary>Adds a new contact.</summary>
         /// <param name="contact">The contact to add.</param>
-        /// <returns>The new contact.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="contact"/> is null.</exception>
         /// <exception cref="ValidationException"><paramref name="contact"/> is invalid.</exception>
         /// <exception cref="ArgumentException">A contact with the same name already exists.</exception>
         Contact Add( Contact contact );
+
+        /// <param name="id">The ID of the game.</param>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="id"/> is less than or equal to 0.</exception>
+        void Delete( int id );
+        /// <summary>Gets a contact.</summary>
+        /// <param name="id">The ID of the contact.</param>
+        /// <returns>The contact, if any.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="id"/> is less than or equal to 0.</exception>
+        Contact Get( int id );
+
+        /// <summary>Gets all contact.</summary>
+        /// <returns>The list of contacts.</returns>
+        IEnumerable<Contact> GetAll();
+
+        /// <summary>Updates an existing contact.</summary>
+        /// <param name="id">The ID of the existing contact.</param>
+        /// <param name="contact">The contact to add.</param>
+        /// <returns>The updated contact.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="contact"/> is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="id"/> is less than or equal to 0.</exception>
+        /// <exception cref="ArgumentException">The game does not exist.
+        /// <para>-or-</para>
+        /// A contact with the same name already exists.
+        /// </exception>
+        /// <exception cref="ValidationException"><paramref name="contact"/> is invalid.</exception>
+        Contact Update( int id, Contact contact );
 
     }
 }
