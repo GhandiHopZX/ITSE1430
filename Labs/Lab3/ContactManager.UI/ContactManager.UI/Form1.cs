17255052eb153a7bb63f8407d393e17d9ab6491d
Solomon Pattee <solomon.pattee@my.tccd.edu>
2019-04-11 22:45:07 -0500

    ok

--- Labs/Lab3/ContactManager.UI/ContactManager.UI/Form1.cs
+++ Labs/Lab3/ContactManager.UI/ContactManager.UI/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Contact_Type;
 
 namespace ContactManager.UI
 {
@@ -21,6 +22,8 @@ namespace ContactManager.UI
         #region
         static bool Alt_F4;
         static bool Fkey;
+
+        public object contact { get; set; }
         #endregion
 
         private void Form1_KeyDown( object sender, KeyEventArgs e )
@@ -55,7 +58,7 @@ namespace ContactManager.UI
             var emu = splitContainer1.Panel1;
             var emos = splitContainer1.Panel2;
 
-            contact += emu.ControlAdded;
+            
             //Can use AddRange now that we don't care about null items
             //var enumor = _games.GetAll();
             //var enumoror = enumor.GetEnumerator();
@@ -67,9 +70,9 @@ namespace ContactManager.UI
             //{
             //};
 
-            var items = .GetAll();
-            items = items.OrderBy();
-            splitContainer1.Panel1. Items.AddRange(items.ToArray());
+            var items = _contacts.GetAll();
+            items = items.OrderBy(GetCName);
+            //emu.;
             //foreach (var game in _games)
             //{
             //    if (game != null)
@@ -77,6 +80,17 @@ namespace ContactManager.UI
             //};
         }
 
+        private IContactDatabase _contacts;
+
+        private string GetCName( Contact contact )
+        {
+            return contact.Name;
+        }
+
+        private string GetCEmail(Contact contact)
+        {
+            return contact.Email; 
+        }
 
         // about form open
         private void AboutToolStripMenuItem_Click( object sender, EventArgs e )
@@ -97,6 +111,5 @@ namespace ContactManager.UI
             var AddCForm = new AddContact();
             AddCForm.ShowDialog();
         }
-        
     }
 }
