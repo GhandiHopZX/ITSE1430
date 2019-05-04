1ed53fc940a105593728e7547c0f2f107e7086c4
Solomon Pattee <solomon.pattee@my.tccd.edu>
2019-05-04 02:07:49 -0500

    ok

---
+++ Labs/Lab4/Nile.Stores.sql/Server.IProductDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile.Stores.sql
+{
+    class Server : IProductDatabase
+    {
+        public Product Add( Product product )
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get( int id )
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove( int id )
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Update( int id, Product product )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
